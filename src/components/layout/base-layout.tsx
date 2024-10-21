d50965342d824cb3e8fe4560913a1297c47cc53a
Ivan Nosov <i.k.nosov@gmail.com>
2024-10-20 22:33:02 -0700

    chore: update htmx version

--- src/components/layout/base-layout.tsx
+++ src/components/layout/base-layout.tsx
@@ -12,9 +12,9 @@ export const BaseLayout = ({ children, title }: PropsWithChildren<BaseLayoutProp
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>{title}</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.10.2/dist/full.min.css" rel="stylesheet" type="text/css" />
-    <script src="https://unpkg.com/htmx.org@1.9.12"
-            integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2"
-            crossorigin="anonymous" />
+    <script src="https://unpkg.com/htmx.org@2.0.3"
+            integrity="sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq"
+            crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com" />
   </head>
   <body>{children}</body>
